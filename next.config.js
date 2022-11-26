5ce6b4c129a9fdf39516187904b64f8d4a74fb29
tmt9 <tienminh920@gmail.com>
2022-11-26 22:36:28 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.hidoglovers.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
